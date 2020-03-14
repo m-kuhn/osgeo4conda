29f6a131f038cc3f00a25743baeaa45a19db4e11
Matthias Kuhn <matthias@opengis.ch>
2020-03-15 00:41:05 +0100

    qfield build

---
+++ conda-recipes/qfield/bld.bat
@@ -0,0 +1,23 @@
+mkdir build
+if errorlevel 1 exit 1
+cd build
+if errorlevel 1 exit 1
+
+set BUILDCONF=Release
+
+set "CMAKE_COMPILER_PATH=%VSINSTALLDIR:\=/%/VC/bin/amd64"
+
+cmake -G Ninja ^
+    -D CMAKE_CXX_COMPILER="%CMAKE_COMPILER_PATH%/cl.exe" ^
+    -D CMAKE_C_COMPILER="%CMAKE_COMPILER_PATH%/cl.exe" ^
+    -D CMAKE_LINKER="%CMAKE_COMPILER_PATH%/link.exe" ^
+    -D CMAKE_BUILD_TYPE=%BUILDCONF% ^
+    -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    ..
+if errorlevel 1 exit 1
+
+ninja -j%CPU_COUNT%
+if errorlevel 1 exit 1
+ninja install
+if errorlevel 1 exit 1
