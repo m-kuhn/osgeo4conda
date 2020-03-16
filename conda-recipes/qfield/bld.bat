4a619ccd59deb4a4d5d30ab0a75a481a7712cc0c
Matthias Kuhn <matthias@opengis.ch>
2020-03-16 09:53:56 +0100

    Do not call anything

--- conda-recipes/qfield/bld.bat
+++ conda-recipes/qfield/bld.bat
@@ -5,8 +5,6 @@ if errorlevel 1 exit 1
 
 set BUILDCONF=Release
 
-call "%VSINSTALLDIR%/VC/vcvarsall.bat" amd64
-
 cmake -G Ninja ^
     -D CMAKE_CXX_COMPILER="%CMAKE_COMPILER_PATH%/cl.exe" ^
     -D CMAKE_C_COMPILER="%CMAKE_COMPILER_PATH%/cl.exe" ^
